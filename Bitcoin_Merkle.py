02f453236c85da3b0d51a866134338b6d18ebe71
0DASAC <jos.montero@uc.cl>
2025-09-01 19:47:50 -0400

    parcial merkel tree ordenado

--- Bitcoin_Merkle.py
+++ Bitcoin_Merkle.py
@@ -2,10 +2,8 @@
 # All trees are constructed with hashes only, and not the raw data
 # If you want to implement them with raw data, you just add one extra level
 
-from optparse import Values
 from hash import *
 import math
-from operator import indexOf
 
 def merkle_parent(hash1, hash2):
     '''Takes the binary hashes and calculates the hash256'''
@@ -192,6 +190,10 @@ class SortedTree:
         '''
         return True
 
+class NodeNonInclusion:
+    def __init__(self, depth, hash):
+        self.depth = depth
+        self.hash = hash
 
 
 class PartialMerkleTree:
@@ -328,38 +330,56 @@ def verify_inclusion(hashesOfInterest, merkleRoot, proof):
     return (tree.root() == merkleRoot)
 
 
+class PartialMerkleTreeNonInclusion:
+    def __init__(self, proof):
+        self.lengthOfLeaves = proof.nrLeaves
+        self.bitFlags = proof.flags
+        self.hashes = proof.hashes
+        self.treeHeight = math.ceil(math.log(self.lengthOfLeaves, 2))
+        self.hashesIndex = {}
+        self.currentIndex = 0
+
+    def GetHash(self, depth):
+        currentBit = self.bitFlags.pop(0)
+        nodeHash = None
+        if (currentBit == 0):
+            difDepth = self.treeHeight - depth
+            self.currentIndex += (2**difDepth)
+            nodeHash = self.hashes.pop(0)
+        elif (depth < self.treeHeight):
+            left = self.GetHash(depth + 1)
+            if (self.currentIndex == self.lengthOfLeaves):
+                nodeHash =  merkle_parent(left, left)
+            else:
+                right = self.GetHash(depth + 1)
+                nodeHash =  merkle_parent(left, right)
+        else:
+            nodeHash = self.hashes.pop(0)
+            self.hashesIndex[nodeHash] = self.currentIndex
+            self.currentIndex += 1
+        return nodeHash
+
 def verify_non_inclusion(hash, merkleRoot, proof):
     '''
     The method receives a hash, a Merkle root, and a proof that hash does not belong to this Merkle root
     the proof is of type MerkleProof
+
     '''
+
+    tree = PartialMerkleTreeNonInclusion(proof)
+    root = tree.GetHash(0)
+    hashesIndex = tree.hashesIndex
+
+    if (merkleRoot != root):
+        return False
+    elif ()
+
+
     return True
 
 
 ## Data for testing:
 
-
-# hex_hashes = [
-#     "9745f7173ef14ee4155722d1cbf13304339fd00d900b759c6f9d58579b5765fb",
-#     "5573c8ede34936c29cdfdfe743f7f5fdfbd4f54ba0705259e62f39917065cb9b",
-#     "82a02ecbb6623b4274dfcab82b336dc017a27136e08521091e443e62582e8f05",
-#     "507ccae5ed9b340363a0e6d765af148be9cb1c8766ccc922f83e4ae681658308",
-#     "a7a4aec28e7162e1e9ef33dfa30f0bc0526e6cf4b11a576f6c5de58593898330",
-#     "bb6267664bd833fd9fc82582853ab144fece26b7a8a5bf328f8a059445b59add",
-#     "ea6d7ac1ee77fbacee58fc717b990c4fcccf1b19af43103c090f601677fd8836",
-#     "457743861de496c429912558a106b810b0507975a49773228aa788df40730d41",
-#     "7688029288efc9e9a0011c960a6ed9e5466581abf3e3a6c26ee317461add619a",
-#     "b1ae7f15836cb2286cdd4e2c37bf9bb7da0a2846d06867a429f654b2e7f383c9",
-#     "9b74f89fa3f93e71ff2c241f32945d877281a6a50a6bf94adac002980aafe5ab",
-#     "b3a92b5b255019bdaf754875633c2de9fec2ab03e6b8ce669d07cb5b18804638",
-#     "b5c0b915312b9bdaedd2b86aa2d0f8feffc73a2d37668fd9010179261e25e263",
-#     "c9d52c5cb1e557b92c84c52e7c4bfbce859408bedffc8a5560fd6e35e10b8800",
-#     "c555bc5fc3bc096df0a0c9532f07640bfb76bfe4fc1ace214b8b228a1297a4c2",
-#     # "f9dbfafc3af3400954975da24eb325e326960a25b87fffe23eef3e7ed2fb610e",
-# ]
-
-
-# tree = MerkleTree(raw_hashes)
 
 # flags=[1,0,1,1,0,1,1,0,1,1,0,1,0]
 # hashes = ["6382df3f3a0b1323ff73f4da50dc5e318468734d6054111481921d845c020b93",
@@ -385,9 +405,6 @@ def verify_non_inclusion(hash, merkleRoot, proof):
 # print(verify_inclusion(r_interest, tree.root, proof))
 
 # proof = tree.generate_proof(r_interest)
-# for hash in proof.hashes:
-#     print(hash.hex())
-# print(proof.flags)
 
 # print(verify_inclusion(r_interest, tree.root, proof))
 
